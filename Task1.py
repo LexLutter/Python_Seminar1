9e846c0cd531ec166d4c4f91c0faf6ec497fd3df
LexLuter <gae_cbr@mail.ru>
2023-04-11 22:54:20 +0700

    Task1 update

--- Task1.py
+++ Task1.py
@@ -1,3 +1,3 @@
-number = 100
-result = int(number / 100) + int((number / 10) - 10) + int(number % 10)
+number = 300
+result = int(number / 100) + int((number / 10) % 10) + int(number % 10)
 print(result)
