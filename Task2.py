5af0a54e882c08538320eed43c5f7ed2a95d6254
LexLuter <gae_cbr@mail.ru>
2023-04-11 22:41:24 +0700

    Task2

---
+++ Task2.py
@@ -0,0 +1,5 @@
+all_cranes = 7
+petya_cranes = int(all_cranes / 6)
+katya_cranes = all_cranes - 2 * petya_cranes
+
+print(f"Всего {all_cranes} журавликов, из них Петя сделал {petya_cranes}, Сережа сделал {petya_cranes}, и Катя сделала {katya_cranes}.")
